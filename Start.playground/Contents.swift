6137fa3c0b0814670c831fd84ce920dfc720febf
divya <divya.lingam@yahoo.com>
2021-03-03 15:39:19 +0530

    Source added

---
+++ Start.playground/Contents.swift
@@ -0,0 +1,44 @@
+//Declaring Constants and Variables
+let count = 10
+var noOfCars = 0
+let pens = 10, bags = 20, fruits = 30
+var dogs = 3,cats=5,rat=6
+let pi = 3.14159//float(upto 6 decimal digits)
+let x = 22.63885319990//double (upto 15 decimal digits)
+var roundIt = Int(pi)
+var xCodeForIOS = true//boolean
+
+//Tuples(grouping the values of differnt types)
+var list = ("Geetha",22,55.3)
+let (name,age,weight)=list
+print("Name is:\(name)")
+print("Age is:\(age)")
+print("Weight is:\(weight)")
+
+print("Name is \(list.0)")
+
+//Annotations
+let jeeps:Int
+jeeps=10
+var favColor:String
+favColor="blue"
+
+//Printing Values
+print (favColor)
+print(pens,bags,fruits)
+
+//if condition
+if weight > 60 {
+    print("You are not fit")
+} else {
+        print("You are fit")
+    }
+
+
+
+
+
+
+
+
+
