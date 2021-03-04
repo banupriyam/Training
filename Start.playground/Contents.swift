b8461ed2dec8b343a75e4ff9fe4bdacd3da320b6
banupriyam <banum1993@gmail.com>
2021-03-04 20:15:17 +0530

    Functions, Control Flow, Closures, Conditionals concepts are completed

--- Start.playground/Contents.swift
+++ Start.playground/Contents.swift
@@ -69,4 +69,74 @@ let convertedNumber =  Int(possibleNumber)
 let userName = "xxx"
 let cuserName: Int? = Int(userName)
 
+//For in Loop
+let babiesList = ["Abi":3,"Karthick":1,"Vijay":5,"Anjali":2,"Ammu":4]
+
+for (babiesName,babiesAge) in (babiesList)
+{
+    print("The name of the baby is : \(babiesName) and the Age is : \(babiesAge)")
+}
+let counT = 10
+for output in 0..<counT
+{
+    print(output)
+}
+
+let minutes = 60
+let minuteInterval = 5
+for tickMark in stride(from: 0, to: minutes, by: minuteInterval)
+{
+print(tickMark)
+}
+
+//While Loop
+
+var a = 5
+var b = 15
+var c = "*"
+while a<b
+ {
+ a = a+1
+    print("\(a)  is  smaller than \(b)")
+}
+
+//Functions
+func greet(person: String) -> String {
+    
+    return  "Hello, " + person + "!"
+}
+print(greet(person: "Ravi"))
+print(greet(person: "Geetha"))
+
+//Function with multiple parameters
+func greetAgain(person: String) -> String {
+    return "Hello again, " + person + "!"
+}
+print(greetAgain(person: "Sam"))
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
+print(greet(person: "Abi", alreadyGreeted: true))
+
+
+
+func addition(num_a: Int, num_b: Int)->Int
+{
+    return num_a + num_b
+}
+print(addition(num_a: 5, num_b: 10))
+
+//Closures
+
+let num = [55,12,0,37,3456,543672,776]
+func ascending(_ numC1: Int, _ numC2: Int) -> Bool {
+    return numC1 < numC2
+}
+var numC = num.sorted(by: ascending)
+print((numC))
+
 
